d85b8184f1e356b0db1cd1f62b3dc0aa22f92267
GCast31 <asgardwyn@gmail.com>
2022-09-25 22:18:31 +0200

    Add pub in common.rs

--- src/game/common.rs
+++ src/game/common.rs
@@ -9,7 +9,7 @@ pub type Transformation = f32;
 pub type DeltaTime = f32;
 
 // 2D
-pub struct Velocity2d(Dimension, Dimension);
-pub struct Point2d(Position, Position);
-pub struct Dimension2d(Dimension, Dimension);
+pub struct Velocity2d(pub Dimension, pub Dimension);
+pub struct Point2d(pub Position, pub Position);
+pub struct Dimension2d(pub Dimension, pub Dimension);
 
