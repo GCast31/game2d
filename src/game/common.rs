5d2794f16fd21944dca38dbe728ab5d4b06eb495
GCast31 <asgardwyn@gmail.com>
2022-09-25 22:21:50 +0200

    Add scale

--- src/game/common.rs
+++ src/game/common.rs
@@ -6,10 +6,13 @@ pub type Fps = f32;
 pub type Position = i32;
 pub type Dimension = u32;
 pub type Transformation = f32;
+pub type Velocity = f32;
 pub type DeltaTime = f32;
 
 // 2D
-pub struct Velocity2d(pub Dimension, pub Dimension);
+pub struct Velocity2d(pub Velocity, pub Velocity);
 pub struct Point2d(pub Position, pub Position);
 pub struct Dimension2d(pub Dimension, pub Dimension);
+
+pub struct Scale2d(pub Transformation, pub Transformation);
 
