1e087ad96b5c7e87ceb3dfc376e3f7d20f495de8
GCast31 <asgardwyn@gmail.com>
2022-10-02 00:46:12 +0200

    Add force

--- src/game/common.rs
+++ src/game/common.rs
@@ -13,6 +13,7 @@ pub type Velocity = f32;
 pub type DeltaTime = f32;
 pub type ColorT = u8;
 pub type FontSize = u16;
+pub type Force = f32;
 
 // 2D
 #[derive(Clone, Copy)]
@@ -23,6 +24,22 @@ pub struct Point2d{ pub x: Position, pub y: Position }
 
 #[derive(Clone, Copy)]
 pub struct Dimension2d{ pub h: Dimension, pub w: Dimension }
+
+#[derive(Clone, Copy)]
+pub struct Force2d{ pub fx: Force, pub fy: Force }
+
+impl Force2d {
+    pub fn new(angle: Angle, speed: Velocity2d) -> Self {
+        let angle_radian = angle.to_radians();
+        let fx: Velocity = (angle_radian.cos() * speed.x as Angle) as Velocity;
+        let fy: Velocity = (angle_radian.sin() * speed.y as Angle) as Velocity;
+        Self {
+            fx,
+            fy,
+        }
+    }
+}
+
 
 #[derive(Clone, Copy)]
 pub struct Scale2d{ pub sx: Transformation, pub sy: Transformation }
