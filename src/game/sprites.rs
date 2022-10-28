03da947e6be44f9cec02752d39c97960af375fb3
GCast31 <asgardwyn@gmail.com>
2022-10-29 01:36:07 +0200

    forward

--- src/game/sprites.rs
+++ src/game/sprites.rs
@@ -1,7 +1,5 @@
 use std::any::{Any, TypeId}; 
-use std::cell::RefCell;
 use std::collections::HashMap;
-use std::rc::Rc;
 
 use crate::graphics::graphics::{Drawable, Graphics};
 use super::game::Updatable;
@@ -10,10 +8,10 @@ use super::common::{DeltaTime, WithSize, WithPosition, Movable};
 
 
 pub trait SpriteTrait: 
-    Any + Drawable + Updatable + WithSize + WithPosition + Movable {}
+    Any +Drawable + Updatable + WithSize + WithPosition + Movable {}
 
 pub struct Sprites {
-    datas: HashMap<TypeId, Vec<Rc<RefCell<dyn SpriteTrait>>>>,
+    datas: HashMap<TypeId, Vec<Box<dyn SpriteTrait>>>,
 }
 
 impl Sprites {
@@ -21,38 +19,38 @@ impl Sprites {
         Self { datas: HashMap::new() }
     }
 
-    pub fn add(&mut self, sprite: impl SpriteTrait) {
+    pub fn add(&mut self, sprite: impl SpriteTrait + 'static) {
         let typeid = sprite.type_id();
         if !self.datas.contains_key(&typeid) {
             self.datas.insert(typeid, Vec::new());
         }
         if let Some(lst) = self.datas.get_mut(&typeid) {
-            lst.push(Rc::new(RefCell::new(sprite)));
+            lst.push(Box::new(sprite));
         }
     }
 
-    pub fn get<T: SpriteTrait>(&self, entity: T) -> Option<&Vec<Rc<RefCell<dyn SpriteTrait>>>> {
+    pub fn get<T: SpriteTrait + 'static>(&self, entity: T) -> Option<&Vec<Box<dyn SpriteTrait>>> {
         let typeid = entity.type_id();
         self.datas.get(&typeid)
     }
 
-    pub fn get_mut<T: SpriteTrait>(&mut self, entity: T) -> Option<&mut Vec<Rc<RefCell<dyn SpriteTrait>>>> {
+    pub fn get_mut<T: SpriteTrait + 'static>(&mut self, entity: T) -> Option<&mut Vec<Box<dyn SpriteTrait>>> {
         let typeid = entity.type_id();
         self.datas.get_mut(&typeid)
     }
 
-    pub fn get_all(&self) -> &HashMap<TypeId, Vec<Rc<RefCell<dyn SpriteTrait>>>> {
+    pub fn get_all(&self) -> &HashMap<TypeId, Vec<Box<dyn SpriteTrait>>> {
         &self.datas
     }
 
-    pub fn get_all_mut(&mut self) -> &mut HashMap<TypeId, Vec<Rc<RefCell<dyn SpriteTrait>>>> {
+    pub fn get_all_mut(&mut self) -> &mut HashMap<TypeId, Vec<Box<dyn SpriteTrait>>> {
         &mut self.datas
     }
 
     pub fn draw(&mut self, graphics: &mut Graphics) {
         for typeid in self.datas.iter_mut() {
             for entity in typeid.1.iter_mut() {
-                entity.borrow_mut().draw(graphics);
+                entity.draw(graphics);
             }
         }
     }
@@ -60,7 +58,7 @@ impl Sprites {
     pub fn update(&mut self, graphics: &mut Graphics, inputs: &mut Inputs, dt: &DeltaTime) {
         for typeid in self.datas.iter_mut() {
             for entity in typeid.1.iter_mut() {
-                entity.borrow_mut().update(graphics, inputs, &dt);
+                entity.update(graphics, inputs, &dt);
             }
         }
     }
